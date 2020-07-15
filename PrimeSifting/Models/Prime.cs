2853e59fa6e7224fdc0105edac7d0e3e94c4c7df
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-15 10:33:55 -0700

    NumberList method test failed
    Co-authored-by: Ian Scott <chesnekov@gmail.com>

--- PrimeSifting/Models/Prime.cs
+++ PrimeSifting/Models/Prime.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+
 namespace PrimeSifting
 {
   public class Prime
   {
+    public static List<int> NumberList(int number)
+    {
+      return new List<int>();
+    }
+    //int number;
+    //Prime.methodToGetPrimes(number);
     // properties, constructors, methods, etc. go here
     
     //private List<int> NumberList(int number)
