8324ee66fc87ddf98dc7c576f3b04838e6e7f940
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-15 10:15:08 -0700

    Prime class constructor test passed
    Co-authored-by: Ian Scott <chesnekov@gmail.com>

--- PrimeSifting/Models/Prime.cs
+++ PrimeSifting/Models/Prime.cs
@@ -1,7 +1,18 @@
-namespace ProjectName
+namespace PrimeSifting
 {
-  public class ClassName
+  public class Prime
   {
     // properties, constructors, methods, etc. go here
+    
+    //private List<int> NumberList(int number)
+    // {
+    //   // Generates list of all numbers
+    // }
+
+    // public list PrimeCalculator()
+    // {
+    //   return primeList;
+    // }
+
   }
 }
